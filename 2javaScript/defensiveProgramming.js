d66f7c35afc25ad4a7ba4ee67163afe4b3d37b38
andrejarboe <andrejarboe@gmail.com>
2023-05-22 10:34:50 -0500

    save

--- 2javaScript/defensiveProgramming.js
+++ 2javaScript/defensiveProgramming.js
@@ -28,3 +28,25 @@ function letterFinder(word, match) {
 letterFinder(1, 2)
 letterFinder([], []);
 letterFinder('cat', 'c');
+
+ var result = null;
+console.log(result);
+ 
+ try {
+		console.log('Hello');
+ } catch (err) {
+		console.log('Goodbye');
+ }
+
+   var x;
+
+		if (x === null) {
+			console.log('null');
+		} else if (x === undefined) {
+			console.log('undefined');
+		} else {
+			console.log('ok');
+}
+        
+  throw new Error();
+	console.log('Hello');
