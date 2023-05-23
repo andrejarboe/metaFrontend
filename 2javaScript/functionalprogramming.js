8045a77b21889e9d1fbb5e6adbb2a86c6259aa3a
andrejarboe <andrejarboe@gmail.com>
2023-05-23 16:36:32 -0500

    save

---
+++ 2javaScript/functionalprogramming.js
@@ -0,0 +1,29 @@
+// Task 1: Build a function-based console log message generator
+function consoleStyler(color, background, fontSize, txt) {
+    var message = '%c' + txt;
+    var style = `color: ${color};`;
+    style += `background: ${background};`;
+    style += `font-size: ${fontSize};`;
+
+    console.log(message);
+    console.log(style);
+}
+
+// Task 2: Build another console log message generator
+function celebrateStyler(reason) {
+    var fontStyle = "color: tomato; font-size: 50px";
+
+    if (reason == 'birthday') {
+        console.log(`%cHappy birthday`, fontStyle);
+    } else if (reason == "champions") {
+        console.log(`%cCongrats on the title!`, fontStyle);
+    } else {
+        console.log(message, style);
+    }
+}
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+consoleStyler('blue', 'red', 16, 'This is text');
+
+// Task 4: Insert a congratulatory and custom message
+function styleAndCelebrate() {}
+// Call styleAndCelebrate
