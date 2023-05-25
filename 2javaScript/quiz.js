ea1c425c056e39e699df296670c93b0482be4ac2
andrejarboe <andrejarboe@gmail.com>
2023-05-25 10:21:18 -0500

    read me

--- 2javaScript/quiz.js
+++ 2javaScript/quiz.js
@@ -1,23 +1,14 @@
-try {
-  throw new Error();
-  console.log('Hello');
-} catch (err) {
-  console.log('Goodbye');
+class Person {
+	sayHello() {
+		console.log('Hello');
+	}
 }
 
-var str = 'Hello';
-str.match('jello');
-
-try {
-  Number(5).toPrecision(300);
-} catch (e) {
-  console.log('There was an error');
+class Friend extends Person {
+	sayHello() {
+		console.log('Hey');
+	}
 }
 
-var globalVar = 77;
-
-function scopeTest() {
-  var localVar = 88;
-}
-
-console.log(localVar);
+var result = new Friend();
+result.sayHello();
