e6752aada1c9832afd9d937aef0483f083ac1af6
andrejarboe <andrejarboe@gmail.com>
2023-05-31 09:12:26 -0500

    save

--- 2javaScript/quiz.js
+++ 2javaScript/quiz.js
@@ -1,6 +1,37 @@
-function count(...basket) {
-	console.log(basket.length);
-}
 
-count(10, 9, 8, 7, 6);
 
+// class Dog  {
+// 	constructor() {
+// 		this.noise = 'bark';
+// 	}
+
+// 	makeNoise() {
+// 		return this.noise;
+// 	}
+// }
+
+// class Wolf extends Dog {
+// 	constructor() {
+// 		super();
+// 		this.noise = 'growl';
+// 	}
+// }
+
+// var result = new Wolf();
+// console.log(result.makeNoise());
+
+// const [a, b] = [1, 2, 3, 4]; 
+// console.log(b);
+
+
+//     function count(...food) {
+// 			console.log(food.length);
+// 		}
+
+// count('Burgers', 'Fries', null);
+		
+
+const letter = 'a';
+letter = 'b';
+
+console.log(letter);
